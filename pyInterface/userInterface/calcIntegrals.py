c1c271abbced556c1dc2a93d8fc75fcba8463e8a
odrotleff <oliver.drotleff@gmail.com>
2014-11-19 12:38:49 +0100

    pyInterface: add calcIntegrals python script
    add userInterface/calcIntegrals.py script. This script calculates an amplitude
    integral matrix from all the amplitude files determined by the file manager.
    Before executing calcIntegrals.py you need to first run calcAmplitudes.py with
    the same fileManager.
    For this change we had to introduce a new package function "calcIntegrals"
    which takes a list of amplitude files as argument and returns the
    ampIntegralMatrix object. The package function extracts all the amplitude trees
    from the amplitude root files and adds the waveNames from the amplitude
    metadata to the ampIntegralMatrix object (this is the new method 'addWave').
    Then calcIntegrals calls the ampIntegralMatrix.integrate method, which has been
    rewritten to take a vector of TTrees as argument (the tree extraction was
    previously done in the ampIntegralMatrix object).

---
+++ pyInterface/userInterface/calcIntegrals.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import argparse
+import sys
+
+import pyRootPwa
+import pyRootPwa.core
+
+if __name__ == "__main__":
+
+	parser = argparse.ArgumentParser(
+	                                 description="generate phase space Monte Carlo events"
+	                                )
+
+	parser.add_argument("outputFileName", type=str, metavar="<outputFile>", help="path to output file")
+	parser.add_argument("-c", type=str, metavar="configFileName", dest="configFileName", default="./rootpwa.config", help="path to config file (default: './rootpwa.config')")
+	parser.add_argument("-i", type=str, metavar="integralName", dest="integralName", default="integral", help="integral TKey name (default: 'integral')")
+	parser.add_argument("-n", type=int, metavar="#", dest="nEvents", default=0, help="maximum number of events to process (default: all)")
+	parser.add_argument("-r", type=int, metavar="#", dest="nEventsRenorm", default=0, help="number of events to renormalize to (default: no renormalization)")
+	parser.add_argument("-w", type=str, metavar="path", dest="weightsFileName", default="", help="path to MC weight file for de-weighting (default: none)")
+	parser.add_argument("-v", "--verbose", help="verbose; print debug output (default: false)", action="store_true")
+	args = parser.parse_args()
+
+	pyRootPwa.utils.stdoutisatty = sys.stdout.isatty()
+	pyRootPwa.utils.stderrisatty = sys.stderr.isatty()
+
+	printErr  = pyRootPwa.utils.printErr
+	printWarn = pyRootPwa.utils.printWarn
+	printSucc = pyRootPwa.utils.printSucc
+	printInfo = pyRootPwa.utils.printInfo
+	printDebug = pyRootPwa.utils.printDebug
+
+	config = pyRootPwa.rootPwaConfig()
+	if not config.initialize(args.configFileName):
+		pyRootPwa.utils.printErr("loading config file '" + args.configFileName + "' failed. Aborting...")
+		sys.exit(1)
+	pyRootPwa.core.particleDataTable.readFile(config.pdgFileName)
+	fileManager = pyRootPwa.loadFileManager(config.fileManagerPath)
+	if not fileManager:
+		pyRootPwa.utils.printErr("loading the file manager failed. Aborting...")
+		sys.exit(1)
+
+	ampFileList = fileManager.getAmpFilePaths()
+	if not ampFileList:
+		printErr("could not retrieve valid amplitude file list. Aborting...")
+		sys.exit(1)
+
+	integral = pyRootPwa.calcIntegrals(ampFileList, args.nEvents, args.weightsFileName)
+
+	outputFile = pyRootPwa.ROOT.TFile.Open(args.outputFileName, "RECREATE")
+	if not outputFile:
+		printErr("cannot open output file '" + args.outputFileName + "'. Aborting...")
+		sys.exit(1)
+
+	nmbBytes = integral.Write(args.integralName)
+	outputFile.Close()
+
+	if nmbBytes == 0:
+		printErr("problems writing integral to TKey '" + args.integralName + "' "
+		         + "in file '" + args.outputFileName + "'")
+		sys.exit(1)
+	else:
+		printSucc("wrote integral to TKey '" + args.integralName + "' "
+		          + "in file '" + args.outputFileName + "'")
