52e67dee695b6daab8c50c8556579d85d27af563
fkrinner <fabian-krinner@mytum.de>
2015-05-20 13:52:03 +0200

    tPrime changes

--- pyInterface/utilities/tPrime_py.cc
+++ pyInterface/utilities/tPrime_py.cc
@@ -8,21 +8,13 @@
 
 namespace bp = boost::python;
 
-double tPrime_py(bp::list beam, bp::list target, bp::list out){
-	std::vector<double> convert;
+double tPrime_py(PyObject* beam, PyObject* target, PyObject* out){
+	TLorentzVector* lvBeam   = rpwa::py::convertFromPy<TLorentzVector*>(beam  );
+	TLorentzVector* lvTarget = rpwa::py::convertFromPy<TLorentzVector*>(target);
+	TLorentzVector* lvOut    = rpwa::py::convertFromPy<TLorentzVector*>(out   );
 
-	rpwa::py::convertBPObjectToVector<double>(beam, convert);
-	TLorentzVector lvBeam = TLorentzVector(&convert[0]);
-
-	rpwa::py::convertBPObjectToVector<double>(target, convert);
-	TLorentzVector lvTarget = TLorentzVector(&convert[0]);
-
-	rpwa::py::convertBPObjectToVector<double>(out, convert);
-	TLorentzVector lvOut = TLorentzVector(&convert[0]);
-
-	return rpwa::tPrime(lvBeam,lvTarget,lvOut);
+	return rpwa::tPrime(*lvBeam,*lvTarget,*lvOut);
 };
-
 
 
 void rpwa::py::exportTprime() {
