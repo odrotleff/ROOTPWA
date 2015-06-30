30cc22c0967b866995128955da85f5fcb5590148
fkrinner <fabian-krinner@mytum.de>
2015-06-30 09:59:25 +0200

    Rebase compiles

---
+++ pyInterface/utilities/tPrime_py.h
@@ -0,0 +1,13 @@
+#ifndef TPRIME_PY
+#define TPRIME_PY
+
+#include<boost/python.hpp>
+
+#include"physUtils.hpp"
+
+namespace rpwa {
+	namespace py {
+		void exportTprime();
+	};
+};
+#endif//TPRIME_PY
