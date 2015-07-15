23b59e20606d63d841f835c4fb8efc13d125c629
Oliver Drotleff <oliver.drotleff@gmail.com>
2015-07-15 16:24:46 +0200

    fix reading of wavelist in nuts sampling (all waves for no wavelist argument)

--- pyInterface/nutsTest.py
+++ pyInterface/nutsTest.py
@@ -10,35 +10,37 @@ from nuts import nuts6
 def readWaveList(waveListFileName, keyFiles):
 	pyRootPwa.utils.printInfo("reading amplitude names and thresholds from wave list file "
 	          + "'" + waveListFileName + "'.")
-	with open(waveListFileName, 'r') as waveListFile:
-#	if (not waveListFile) {
-#		printErr << "cannot open file '" << waveListFileName << "'. Aborting..." << endl;
-#		throw;
-#	}
-		waveDescThres = []
-		lineNmb = 0
-		for line in waveListFile:
-			if (line[0] == '#'):  # comments start with #
-				continue
-			line = line.replace('\n', '')
-			lineArray = line.split(" ")
-			if(len(lineArray) >= 1 and len(lineArray) <= 2):
-				waveName = lineArray[0]
-				if(len(lineArray) == 1):
-					threshold = 0
+	waveDescThres = []
+	if waveListFileName == '':
+		for waveName in keyFiles:
+			waveDesc = pyRootPwa.core.waveDescription()
+			waveDesc.parseKeyFile(keyFiles[waveName])
+			waveDescThres.append( (waveName, waveDesc, 0.) )
+	else:
+		with open(waveListFileName, 'r') as waveListFile:
+			lineNmb = 0
+			for line in waveListFile:
+				if (line[0] == '#'):  # comments start with #
+					continue
+				line = line.replace('\n', '')
+				lineArray = line.split(" ")
+				if(len(lineArray) >= 1 and len(lineArray) <= 2):
+					waveName = lineArray[0]
+					if(len(lineArray) == 1):
+						threshold = "0"
+					else:
+						threshold = lineArray[1]
+					waveDesc = pyRootPwa.core.waveDescription()
+					waveDesc.parseKeyFile(keyFiles[waveName])
+					waveDescThres.append( (waveName, waveDesc, float(threshold)) )
 				else:
-					threshold = lineArray[1]
-				waveDesc = pyRootPwa.core.waveDescription()
-				waveDesc.parseKeyFile(keyFiles[waveName])
-				waveDescThres.append( (waveName, waveDesc, float(threshold)) )
-			else:
-				pyRootPwa.utils.printWarn("cannot parse line '" + line + "' in wave list file "
-				          + "'" + waveListFileName + "'.")
-#  			if (_debug):
-#  				printDebug("reading line " + lineNmb + 1 + ": " + waveName + ", "
-#  				           + "threshold = " + threshold + " MeV/c^2")
-			lineNmb += 1
-	pyRootPwa.utils.printInfo("read " + str(lineNmb) + " lines from wave list file " + "'" + waveListFileName + "'")
+					pyRootPwa.utils.printWarn("cannot parse line '" + line + "' in wave list file "
+					          + "'" + waveListFileName + "'.")
+	#  			if (_debug):
+	#  				printDebug("reading line " + lineNmb + 1 + ": " + waveName + ", "
+	#  				           + "threshold = " + threshold + " MeV/c^2")
+				lineNmb += 1
+			pyRootPwa.utils.printInfo("read " + str(lineNmb) + " lines from wave list file " + "'" + waveListFileName + "'")
 	return waveDescThres
 
 def getStartValues(likelihood, seed):
